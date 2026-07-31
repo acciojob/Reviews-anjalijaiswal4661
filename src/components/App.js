5cb7b44b5032a13a75380f4a7cd9c6d6714c734f
unknown <itsanjali390@gmail.com>
2026-07-31 17:15:32 +0530

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,16 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+import Review from "./Review";
 
 function App() {
-  const [showPara, setShowPara] = useState(false);
-
-  const handleClick = () => {
-    setShowPara(true);
-  };
-
   return (
-    <div id="main">
-      <p
-        id="para"
-        className={showPara ? "show" : "hide"}
-      >
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-      </p>
-
-      <button id="click" onClick={handleClick}>
-        Click Me
-      </button>
-    </div>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2 id="review-heading">Our Reviews</h2>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 }
 
